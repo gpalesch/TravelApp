10a55dcbd226cf233f689cb655aa23cc41131c7c
Gina Palesch <gpalesch@bynativ.com>
2024-10-27 16:52:38 +0100

    Initial projet structure

---
+++ destination-service/src/test/java/com/example/destination_service/DestinationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.destination_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
